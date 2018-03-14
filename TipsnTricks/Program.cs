e0d95b372200ceb4cf3cd156d74ab426da3e1ea2
Michael Jepson <Michael.Jepson@cmtelecom.com>
2018-03-14 11:38:51 +0100

    Add project files.

---
+++ TipsnTricks/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TipsnTricks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
